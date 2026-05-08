f95bf892f5592255c7e2dddce1ecf781662976d1
rodolfo-dev-git <rodolfodevgit@gmail.com>
2026-05-08 12:07:11 -0300

    Inserindo animção no titulo

--- script.js
+++ script.js
@@ -2,18 +2,37 @@ const result = document.querySelector('.result');
 const myScore = document.querySelector('#human-score');
 const machineScore = document.querySelector('#machine-score');
 
+const title = document.querySelector('.title');
+
 let scorePlayer = 0;
 let scoreMachine = 0;
+
+
 
 const playHuman = (choiceHuman) => {
     console.log(choiceHuman);
 
-    playGame(choiceHuman, playMachine())
+    result.innerHTML = "";
+    animateTitle();
+
+
+
+
+    console.log("pronto")
+    // playGame(choiceHuman, playMachine());
+
+    const machineChoice = playMachine();
+
+    setTimeout(() => {
+        playGame(choiceHuman, machineChoice);
+    }, 900);
 }
 
 const playMachine = () => {
     const choices = ['rock', 'paper', 'scissors'];
-    const randomNumber = Math.floor(Math.random() * 3)
+    const randomNumber = Math.floor(Math.random() * 3);
+
+
 
     return choices[randomNumber];
 }
@@ -22,12 +41,13 @@ const playMachine = () => {
 const playGame = (human, machine) => {
     console.log('Humano: ' + human + " Maquina: " + machine)
 
+
     if (human === machine) {
         result.innerHTML = "Deu empate!";
     } else if ((human === 'paper' && machine === 'rock') ||
         (human === 'scissors' && machine === 'paper') ||
         (human === 'rock' && machine === 'scissors')) {
-        scorePlayer ++;
+        scorePlayer++;
         myScore.innerHTML = scorePlayer;
         result.innerHTML = "Você ganhou!";
     } else {
@@ -36,3 +56,17 @@ const playGame = (human, machine) => {
         result.innerHTML = "A maquina ganhou!";
     }
 }
+
+
+const animateTitle = () => {
+    title.innerHTML = "Jo";
+
+    setTimeout(() => {
+        title.innerHTML = "Joken";
+    }, 300);
+
+    setTimeout(() => {
+        title.innerHTML = "JokenPô";
+    }, 600);
+}
+
